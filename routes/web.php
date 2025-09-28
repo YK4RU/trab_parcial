5af0f20baf3470c8e841557f4ea653fceec194a3
YK4RU <ana.lima1801@aluno.ce.gov.br>
2025-09-28 14:48:56 -0300

    feat: modificação do arquivo adicionando as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\NameController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/names', [NameController::class, 'index'])->name('names.index');
+
+Route::get('/names/create', [NameController::class, 'create'])->name('names.create');
+Route::post('/names', [NameController::class, 'store'])->name('names.store');
+Route::get('/names/{name}', [NameController::class, 'show'])->name('names.show');
+Route::get('/names/{name}/edit', [NameController::class, 'edit'])->name('names.edit');
+Route::put('/names/{name}', [NameController::class, 'update'])->name('names.update');
+Route::delete('/names/{name}', [NameController::class, 'destroy'])->name('names.destroy');
