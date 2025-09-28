bc1ab74faf67e625534ca65ec70c693b8538394c
YK4RU <ana.lima1801@aluno.ce.gov.br>
2025-09-28 14:51:02 -0300

    feat: adicionando o formulário de criação

---
+++ resources/views/names/create.blade.php
@@ -0,0 +1,16 @@
+<h1>Cadastro de frases</h1>
+
+@if (session()->has('message'))
+    {{ session()->get('message') }}
+@endif
+
+<form action="{{ route('names.store')}}" method="post">
+    @csrf
+    <p>
+        <label for="name">Digite uma frase</label>
+        <input type="text" name="name" id="name">
+    </p>
+    <p>
+        <input type="submit" value="Entrar" id="botaoEntrar">
+    </p>
+</form>
